d841cdad9c262f6df77d0ede19ea1436d1cc3de2
jensvdsmct <jens.vanderstraeten@student.howest.be>
2024-03-06 10:26:34 +0100

    feat: dim todos when done, reset input, delete function & filter todocount

--- src/screens/TodoOverview.tsx
+++ src/screens/TodoOverview.tsx
@@ -1,4 +1,4 @@
-import { Plus, Check, Settings } from 'lucide-react'
+import { Plus, Check, Settings, Trash2 } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { uid } from 'uid'
 
@@ -7,11 +7,28 @@ import { AppFooter } from '../components/AppFooter'
 import { AppHeader } from '../components/AppHeader'
 import { Link } from 'react-router-dom'
 
-// TODO 1: als er niets ingevuld is, mag de todo niet toegevoegd worden
-// TODO 2: een geldige todo krijgt een unieke id (met de npm uid package)
-
 export const TodoOverview = () => {
-  const [todos, setTodos] = useState<Todo[]>(localStorage.todos ? JSON.parse(localStorage.todos) : [])
+  // TODO: fade todo when checked
+  // TODO: option to delete a todo
+  // TODO: show error message when input fields are empty
+  // TODO: make the input fields required (input validation - visible)
+  //
+  // TODO: Release better version (v1.1.0)
+
+  const [isValid, setIsValid] = useState({
+    task: {
+      dirty: false, // Has the user interacted with the input field?
+      valid: false,
+    },
+    category: {
+      dirty: false,
+      valid: false,
+    },
+  })
+
+  const [todos, setTodos] = useState<Todo[]>(
+    localStorage.todos ? JSON.parse(localStorage.todos) : [],
+  )
   const [newTodo, setNewTodo] = useState<Todo>({
     task: '',
     category: 'choose',
@@ -32,6 +49,27 @@ export const TodoOverview = () => {
       setTodos([...todos, currentNewTodo]) // Combineer de huidige todos met de nieuwe todo
       return currentNewTodo
     }) // Maak een unique id aan voor het opslaan van deze nieuwe todo
+
+    setNewTodo({
+      task: '',
+      category: 'choose',
+      isCompleted: false,
+    }) // Reset de input velden
+  }
+
+  const toggleTodo = (id: string) => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: !todo.isCompleted }
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  const deleteTodo = (id: string) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+    setTodos(updatedTodos)
   }
 
   useEffect(() => {
@@ -47,10 +85,12 @@ export const TodoOverview = () => {
     )
   }
 
+  const completedTodos = todos.filter(todo => !todo.isCompleted).length
+
   return (
     <div className=" container mx-auto rounded-lg p-10 bg-violet-50 shadow-lg dark:bg-violet-800">
       <div className="flex justify-between p-1">
-        <AppHeader title={title()} todoCount={todos.length} />
+        <AppHeader title={title()} todoCount={completedTodos} />
         <div className="flex justify-end">
           <Link
             to="/settings"
@@ -65,7 +105,7 @@ export const TodoOverview = () => {
           <div className="grid grid-cols-6 gap-3">
             <div className="flex items-center justify-center h-full">
               <button className="text-violet-700 hover:text-violet-300 rounded-full border-2 border-violet-700 hover:border-violet-300 dark:text-violet-300 dark:border-violet-300 dark:hover:text-violet-700 dark:hover:border-violet-700 flex items-center justify-center">
-                <Plus size={30} />
+                <Plus size={40} />
                 <span className="sr-only">Add todo</span>
               </button>
             </div>
@@ -113,21 +153,38 @@ export const TodoOverview = () => {
           {todos.map((todo: Todo) => (
             <div
               key={todo.id}
-              className="flex items-center mb-4 p-3 bg-violet-100 rounded-lg dark:bg-violet-700"
+              className={`
+              flex items-center mb-4 p-3 bg-violet-100 rounded-lg dark:bg-violet-700 $ ${
+                todo.isCompleted ? 'opacity-50' : ''
+              }`}
             >
-              <input type="checkbox" id={todo.id} className="mr-3 hidden" />
               <label htmlFor={todo.id} className="flex items-center">
-                <div className="w-8 h-8 rounded-full border-2 border-violet-500 dark:border-violet-300 mr-5 p-1 text-violet-700 dark:text-violet-300">
-                  <Check
-                    size={20}
-                    className="mr-2 text-violet-500 dark:text-violet-300"
-                  />
-                </div>
-                <div>
-                  <p className="font-bold text-lg">{todo.task}</p>
-                  <p className="text-base text-violet-950">{todo.category}</p>
+                <input
+                  type="checkbox"
+                  id={todo.id}
+                  className="mr-3 hidden"
+                  onChange={() => toggleTodo(todo.id!)}
+                  checked={todo.isCompleted}
+                />
+                <div className="flex items-center">
+                  <div className="text-violet-700 hover:text-violet-300 rounded-full border-2 border-violet-700 hover:border-violet-300 dark:text-violet-300 dark:border-violet-300 dark:hover:text-violet-700 dark:hover:border-violet-700 flex items-center justify-center mr-3 w-6 h-6">
+                    {todo.isCompleted && <Check size={24} />}
+                    <span className="sr-only">Mark as completed</span>
+                  </div>
+                  <div>
+                    <p className="font-bold text-lg">{todo.task}</p>
+                    <p className="text-base text-violet-950">{todo.category}</p>
+                  </div>
                 </div>
               </label>
+
+              <button
+                className="ml-auto text-violet-700 hover:text-violet-300 dark:text-violet-300 dark:hover:text-violet-700"
+                onClick={() => deleteTodo(todo.id!)}
+              >
+                <Trash2 />
+                <span className="sr-only">Delete todo</span>
+              </button>
             </div>
           ))}
         </div>
