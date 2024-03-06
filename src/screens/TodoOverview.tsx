9c796575d50000c65d2be4f0a6ec8e012dba00c6
jensvdsmct <jens.vanderstraeten@student.howest.be>
2024-03-06 10:38:53 +0100

    feat: finished basic input validation

--- src/screens/TodoOverview.tsx
+++ src/screens/TodoOverview.tsx
@@ -104,7 +104,10 @@ export const TodoOverview = () => {
         <form className="my-4" onSubmit={addNewTodo}>
           <div className="grid grid-cols-6 gap-3">
             <div className="flex items-center justify-center h-full">
-              <button className="text-violet-700 hover:text-violet-300 rounded-full border-2 border-violet-700 hover:border-violet-300 dark:text-violet-300 dark:border-violet-300 dark:hover:text-violet-700 dark:hover:border-violet-700 flex items-center justify-center">
+              <button
+                className="text-violet-700 hover:text-violet-300 rounded-full border-2 border-violet-700 hover:border-violet-300 dark:text-violet-300 dark:border-violet-300 dark:hover:text-violet-700 dark:hover:border-violet-700 flex items-center justify-center disabled:opacity-20 disabled:cursor-not-allowed"
+                disabled={!isValid.task.valid || !isValid.category.valid}
+              >
                 <Plus size={40} />
                 <span className="sr-only">Add todo</span>
               </button>
@@ -117,24 +120,46 @@ export const TodoOverview = () => {
                   id="new-todo"
                   placeholder="Add a new todo"
                   value={newTodo.task}
-                  onInput={(event: React.FormEvent<HTMLInputElement>) =>
+                  onInput={(event: React.FormEvent<HTMLInputElement>) => {
+                    setIsValid({
+                      ...isValid,
+                      task: {
+                        dirty: true,
+                        valid: event.currentTarget.value.length > 0,
+                      },
+                    })
                     setNewTodo({ ...newTodo, task: event.currentTarget.value })
-                  }
-                  className="w-full bg-violet-100 rounded-lg p-2 dark:bg-violet-700 border-2 border-transparent dark:border-violet-300 focus:outline-none focus-visible:border-violet-500 hover:border-violet-400 dark:hover:border-violet-300"
-                />
+                  }}
+                  className={`w-full bg-violet-100 rounded-lg p-2 dark:bg-violet-700 border-2 border-transparent dark:border-violet-300 focus:outline-none focus-visible:border-violet-500 hover:border-violet-400 dark:hover:border-violet-300 ${
+                    !isValid.task.valid && isValid.task.dirty
+                      ? 'border-red-500 placeholder:text-red-500 focus:ring-red-700'
+                      : ''
+                  }`}
+                ></input>
               </div>
               <div>
                 <select
                   name="category"
                   id="category"
-                  className="w-full bg-violet-100 rounded-lg p-2 dark:bg-violet-700 border-2 border-transparent dark:border-violet-300 focus:outline-none focus-visible:border-violet-500 hover:border-violet-400 dark:hover:border-violet-300"
+                  className={`w-full bg-violet-100 rounded-lg p-2 dark:bg-violet-700 border-2 border-transparent dark:border-violet-300 focus:outline-none focus-visible:border-violet-500 hover:border-violet-400 dark:hover:border-violet-300 ${
+                    !isValid.category.valid && isValid.category.dirty
+                      ? 'border-red-500 placeholder:text-red-500 focus:ring-red-700'
+                      : ''
+                  }`}
                   value={newTodo.category}
-                  onChange={(event: React.FormEvent<HTMLSelectElement>) =>
+                  onChange={(event: React.FormEvent<HTMLSelectElement>) => {
+                    setIsValid({
+                      ...isValid,
+                      category: {
+                        dirty: true,
+                        valid: event.currentTarget.value !== 'choose',
+                      },
+                    })
                     setNewTodo({
                       ...newTodo,
                       category: event.currentTarget.value,
                     })
-                  }
+                  }}
                 >
                   <option disabled value={'choose'}>
                     Choose a category.
